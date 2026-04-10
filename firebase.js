5325144eaa8d69a49908c245a1cc91f9db0bafdc
bomandy2222-hue <bomandy2222@gmail.com>
2026-04-10 20:16:00 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,14 @@
-https://github.com/bomandy2222-hue/velaxion-app/new/main?filename=src/firebase.js
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "여기에 너 API KEY",
+  authDomain: "velaxion-web.firebaseapp.com",
+  projectId: "velaxion-web",
+  storageBucket: "velaxion-web.appspot.com",
+  messagingSenderId: "945728456758",
+  appId: "1:945728456758:web:f9004c4747fe49fdbc7097"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
