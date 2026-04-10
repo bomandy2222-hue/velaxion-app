18d30bdc0380814449d7864c2394c10301da1dd8
bomandy2222-hue <bomandy2222@gmail.com>
2026-04-10 20:12:08 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,6 @@
-https://github.com/bomandy2222-hue/velaxion-app/new/main?filename=vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+})
