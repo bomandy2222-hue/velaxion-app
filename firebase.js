02466c176785b3bf5fc15fda65aa1bcd5aefc59b
bomandy2222-hue <bomandy2222@gmail.com>
2026-04-16 02:42:19 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,9 +4,9 @@ const firebaseConfig = {
   apiKey: "AIzaSyACM-KtBuWZoDgV-CDq-9XXIOJY0Wx9-uU",
   authDomain: "velaxion-web.firebaseapp.com",
   projectId: "velaxion-web",
-  storageBucket: "velaxion-web.firebasestorage.app",
+  storageBucket: "velaxion-web.appspot.com",
   messagingSenderId: "945728456758",
-  appId: "1:945728456758:web:f9004c4747fe49fdbc7097"
+  appId: "1:945728456758:web:f9004c4747fe49fdbc7097",
 };
 
 const app = initializeApp(firebaseConfig);
